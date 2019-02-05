72f0c8c44be6300958e268d932fbaf01d1d7474b
OAMPfed <44833979+OAMPfed@users.noreply.github.com>
2019-02-05 18:22:37 +0100

    My finished work

---
+++ 050219exercise.js
@@ -0,0 +1,10 @@
+function Mammal(species, diet) { 
+    this.species = species;
+    this.diet = diet;
+}
+          
+Mammal.prototype.type = "Mammal";
+Mammal.prototype = function(description) {
+    console.log("A " + ${Mammal.species} " is a " ${Mammal.diet})
+}
+
